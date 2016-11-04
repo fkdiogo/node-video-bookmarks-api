c2344e5e751036a9906111153a0a8f135b0cadf4
mrholeechit <fkdiogo@gmail.com>
2016-11-04 15:57:11 -0200

    adding some files of the database

---
+++ db/client.js
@@ -0,0 +1,5 @@
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+
+var url = 'mongodb://localhost:27017/videodb';
+
---
+++ schemas/tag.js
@@ -0,0 +1,5 @@
+'use strict';
+var mongoose = require('mongoose');
+var tagSchema = mongoose.Schema({
+    name: String
+});
